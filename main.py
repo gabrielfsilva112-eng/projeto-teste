127127352816d789f863a44ac0f542d5614278e6
gabrielfsilva112-eng <gabriel.f.silva112@aluno.senai.br>
2026-03-06 10:10:32 -0300

    atualizei arquivos

--- main.py
+++ main.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def index():
     return "<h1>Hello World!<h1>"
 
+@app.route("/curso")
+def curso():
+    return '<h1>Desenvolvimento de Sistemas<h1>' \
+    'Gabriel Ferreira'
+
 if __name__ == "__main__":
     app.run()
